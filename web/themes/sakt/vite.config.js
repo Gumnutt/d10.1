663ecdaadd0b650dbd9f501ad355139808aaf392
Brynn Briedis <brynn@pragma.com.au>
2023-07-06 10:57:19 +1000

    flippin shit

--- web/themes/sakt/vite.config.js
+++ web/themes/sakt/vite.config.js
@@ -1,36 +1,45 @@
 import liveReload from "vite-plugin-live-reload"
 import vue from "@vitejs/plugin-vue"
 import { splitVendorChunkPlugin } from "vite"
+import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
 
 export default {
   plugins: [
-    vue({
-      compilerOptions: {
-        isCustomElement: (tag) => {
-          return tag.startsWith("ds")
-        },
-      },
-    }),
+    // vue({
+    //   compilerOptions: {
+    //     isCustomElement: (tag) => {
+    //       return tag.startsWith("ds")
+    //     },
+    //   },
+    // }),
     liveReload(__dirname + "/**/*.(php|inc|theme|twig|js|css|pcss)"),
     // splitVendorChunkPlugin(),
   ],
 
-  resolve: { alias: { vue: "vue/dist/vue.esm-bundler.js" } },
+  // resolve: { alias: { vue: "vue/dist/vue.esm-bundler.js" } },
 
   build: {
     // generate manifest.json in outDir
-    manifest: true,
+    manifest: false,
     emptyOutDir: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: [
-        "/src/main.js",
-      ],
-      // Remove the [hash] since Drupal will take care of that.
+      input: {
+        components: "./components/**/*.pcss",
+        main: "/src/main.js",
+      },
       output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `chunks/[name].[hash].js`,
-        assetFileNames: `[name].[ext]`,
+        assetFileNames: (assetInfo) => {
+          console.log(assetInfo);
+          //const inputName = Object.keys(assetInfo.rollupEntrypoint)[0]; // Get the input name associated with the output file
+          if (assetInfo.name === `${inputName}.css`) {
+            // Output the component CSS file to the component-specific directory
+            return `components/${inputName}/${assetInfo.name}`;
+          } else {
+            // Output other files (main.pcss, assets) to the standard dist directory
+            return `dist/${assetInfo.name}`;
+          }
+        }
       },
     },
   },
