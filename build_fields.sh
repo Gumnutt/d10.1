5882afee6ec670c2f38434af96c78134a7ec64cf
Alex M <alex.monaghan@pragma.com.au>
2023-07-20 11:01:08 +1000

    cleanup

---
+++ build_fields.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define your array of fields
+declare -a fields=("project_proponent" "project_name" "project_id" "method" "method_url" "method_type" "project_description" "date_project_registered" "project_location" "project_location_postcode" "project_area" "project_mapping_file_url" "carbon_est_area_mapping_file_url" "carbon_est_area_sup_info" "regional_nrm_plan" "joint_implementation_project" "carbon_maintenance_obligation" "conditional_reg_approvals" "conditional_rel_interest_holders" "permanence_period" "permanence_period_finish_date" "contracted_on" "contract_id" "accus_total_units_issued" "kaccus_total_units_issued" "kaccus_units_2012_13" "kaccus_units_2013_14" "kaccus_units_2014_15" "kaccus_units_2015_16" "kaccus_units_2016_17" "kaccus_units_2017_18" "kaccus_units_2018_19" "kaccus_units_2019_20" "kaccus_units_2020_21" "kaccus_units_2021_22" "kaccus_units_2022_23" "kaccus_units_2023_24" "kaccus_units_issued_person" "kaccus_units_relinquished" "nkaccus_total_units_issued" "nkaccus_units_2012_13" "nkaccus_units_2013_14" "nkaccus_units_2014_15" "nkaccus_units_2015_16" "nkaccus_units_2016_17" "nkaccus_units_2017_18" "nkaccus_units_2018_19" "nkaccus_units_2019_20" "nkaccus_units_2020_21" "nkaccus_units_2021_22" "nkaccus_units_2022_23" "nkaccus_units_2023_24" "nkaccus_units_issued_person" "nkaccus_units_relinquished" "notes")
+for field in "${fields[@]}"
+do
+    lando drush field-create node dataset --field-name=$field --field-label=$field --field-type=string_long --field-widget=text_textarea --is-required=1 --cardinality=1 --field-description=
+done
---
+++ web/modules/custom/alex_csv_migrate/src/Plugin/migrate/process/CustomCsvFields.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\alex_csv_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Adds custom CSV fields dynamically to the migration process.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "custom_csv_fields"
+ * )
+ */
+class CustomCsvFields extends ProcessPluginBase {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $columns = $this->configuration['columns'];
+    $field_values = [];
+    foreach ($columns as $column) {
+      dump($column);
+      $field_name = 'field_' . $column;
+      $field_value = $row->getSourceProperty($column);
+      $field_values[$field_name] = [
+        'plugin' => 'custom_csv_source',
+        'source' => $column,
+      ];
+    }
+    return $field_values;
+  }
+}
