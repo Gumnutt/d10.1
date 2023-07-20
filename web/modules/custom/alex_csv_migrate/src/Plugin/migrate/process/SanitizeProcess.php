8c90ea671606fa770cadc0224570625e83cd3c5a
Alex M <alex.monaghan@pragma.com.au>
2023-07-20 14:56:07 +1000

    pre migrate setup

--- web/modules/custom/alex_csv_migrate/src/Plugin/migrate/process/SanitizeProcess.php
+++ web/modules/custom/alex_csv_migrate/src/Plugin/migrate/process/SanitizeProcess.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Drupal\alex_csv_migrate\Plugin\migrate\process;
+namespace Drupal\custom_migrate\Plugin\migrate\process;
 
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
 /**
- * Sanitize process plugin to remove HTML tags from the source data.
+ * Sanitize process plugin to remove HTML tags and convert to UTF-8.
  *
  * @MigrateProcessPlugin(
  *   id = "sanitize_process"
@@ -19,6 +19,9 @@ class SanitizeProcess extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    // Convert the value to UTF-8 (if it's not already).
+    $value = mb_convert_encoding($value, 'UTF-8', 'auto');
+
     // Remove any HTML tags from the source data using strip_tags().
     return strip_tags($value);
   }
