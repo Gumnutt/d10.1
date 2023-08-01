54092d8f5417e4b10dc769da3f3acda8079104f2
Brynn Briedis <brynn@pragma.com.au>
2023-08-01 10:05:45 +1000

    adding map component

--- web/themes/sakt/webpack.mix.js
+++ web/themes/sakt/webpack.mix.js
@@ -1,32 +1,33 @@
-let mix = require('laravel-mix');
-let path = require('path');
-let glob = require('glob');
-let LiveReloadWebpackPlugin = require('@kooneko/livereload-webpack-plugin');
+let mix = require("laravel-mix")
+let path = require("path")
+let glob = require("glob")
+let LiveReloadWebpackPlugin = require("@kooneko/livereload-webpack-plugin")
 
 mix.webpackConfig({
-  plugins: [new LiveReloadWebpackPlugin()]
-});
+  plugins: [new LiveReloadWebpackPlugin()],
+})
 mix.options({
-  postCss: [
-    require("postcss-nested"),
-    require("postcss-custom-media"),
-    require("autoprefixer"),
-  ]
-});
+  postCss: [require("postcss-nested"), require("postcss-custom-media"), require("autoprefixer")],
+})
 
 // compile component styles
-let pcssFiles = glob.sync('components/**/*.pcss');
+let pcssFiles = glob.sync("components/**/*.pcss")
 
 pcssFiles.forEach((pcssFile) => {
-  let outputDirectory = path.dirname(pcssFile);
-  mix.postCss(pcssFile, outputDirectory, [
-    require('postcss-import'),
-    require('autoprefixer'),
-  ]);
-});
+  let outputDirectory = path.dirname(pcssFile)
+  mix.postCss(pcssFile, outputDirectory, [require("postcss-import"), require("autoprefixer")])
+})
 
 // compile global styles
-mix.postCss('src/css/main.css', 'dist');
+mix.postCss("src/css/main.css", "dist")
+
+mix.js("src/js/vue.js", "dist").vue({
+  options: {
+    compilerOptions: {
+      isCustomElement: (tag) => ["MapboxMap"].includes(tag),
+    },
+  },
+})
 
 // mix.browserSync({
 //   proxy: 'https://drupal.lndo.site',
